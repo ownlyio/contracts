80c9b3d5e7b2c44e37f66832c13bed7e3cf6dc49
Bernard Historillo <bernardhistorillo1@gmail.com>
2021-07-27 10:39:40 +0800

    Use Hardhat for Marketplace development and make contract upgradeable

--- ERC20/Ownly.sol
+++ ERC20/Ownly.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.8.0;
 import "./ERC20PresetFixedSupply.sol";
 
 contract Ownly is ERC20PresetFixedSupply {
-    constructor() ERC20PresetFixedSupply ("Ownly", "OWN", 10000000000 * 10**18, 0x768532c218f4f4e6E4960ceeA7F5a7A947a1dd61) {}
+    constructor() ERC20PresetFixedSupply ("Ownly", "OWN", 10000000000 * 10**18, 0x672b733C5350034Ccbd265AA7636C3eBDDA2223B) {}
 }
---
+++ marketplace/contracts/Marketplace.sol
@@ -0,0 +1,245 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
+
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+abstract contract NftInterface {
+    function getApproved(uint256 tokenId) public view virtual returns (
+        address approved
+    );
+    function ownerOf(uint256 tokenId) public view virtual returns (
+        address owner
+    );
+}
+
+contract Marketplace is Initializable, OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    CountersUpgradeable.Counter private _itemIds;
+    CountersUpgradeable.Counter private _itemsSold;
+
+    address payable marketplaceOwner;
+    uint256 listingPrice;
+
+    struct MarketItem {
+        uint itemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool cancelled;
+    }
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    event MarketItemCreated (
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        uint256 price
+    );
+
+    event MarketItemCancelled (
+        uint indexed itemId
+    );
+
+    event MarketItemSold (
+        uint indexed itemId
+    );
+
+    function initialize() initializer public {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+
+        marketplaceOwner = payable(msg.sender);
+        listingPrice = 0;
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+    internal
+    onlyOwner
+    override
+    {}
+
+    function getListingPrice() public view virtual returns (uint256) {
+        return listingPrice;
+    }
+
+    function setListingPrice(uint256 _listingPrice) public virtual {
+        listingPrice = _listingPrice;
+    }
+
+    function getMarketItem(uint256 marketItemId) public view virtual returns (MarketItem memory) {
+        return idToMarketItem[marketItemId];
+    }
+
+    function createMarketItem(
+        address nftContractAddress,
+        uint256 tokenId,
+        uint256 price
+    ) public virtual payable nonReentrant {
+        NftInterface nftContract = NftInterface(nftContractAddress);
+        address nftOwner = nftContract.ownerOf(tokenId);
+
+        require(nftOwner == msg.sender, "You must be the owner of the token");
+        require(price > 0, "Price must be at least 1 wei");
+        require(msg.value == listingPrice, "Value must be equal to listing price");
+        require(unsoldMarketItemExists(nftContractAddress, tokenId) == false, "Market item already exists");
+
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        idToMarketItem[itemId] =  MarketItem(
+            itemId,
+            nftContractAddress,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        emit MarketItemCreated(
+            itemId,
+            nftContractAddress,
+            tokenId,
+            msg.sender,
+            price
+        );
+    }
+
+    function createMarketSale(
+        uint256 itemId
+    ) public virtual payable nonReentrant {
+        uint price = idToMarketItem[itemId].price;
+        uint tokenId = idToMarketItem[itemId].tokenId;
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721Upgradeable(idToMarketItem[itemId].nftContract).transferFrom(idToMarketItem[itemId].seller, msg.sender, tokenId);
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        _itemsSold.increment();
+
+        if(listingPrice > 0) {
+            payable(marketplaceOwner).transfer(listingPrice);
+        }
+
+        emit MarketItemSold(
+            itemId
+        );
+    }
+
+    function cancelMarketSale(
+        uint256 itemId
+    ) public virtual {
+        NftInterface nftContract = NftInterface(idToMarketItem[itemId].nftContract);
+        address nftOwner = nftContract.ownerOf(idToMarketItem[itemId].tokenId);
+
+        require(nftOwner == msg.sender, "You are not the owner of this token.");
+
+        idToMarketItem[itemId].cancelled = false;
+
+        emit MarketItemCancelled(
+            itemId
+        );
+    }
+
+    function unsoldMarketItemExists(address nftContractAddress, uint256 tokenId) internal view virtual returns (bool) {
+        MarketItem[] memory items = fetchMarketItems();
+        bool exists = false;
+
+        for (uint i = 0; i < items.length; i++) {
+            if(items[i].nftContract == nftContractAddress && items[i].tokenId == tokenId) {
+                exists = true;
+                break;
+            }
+        }
+
+        return exists;
+    }
+
+    function fetchMarketItem(address nftContractAddress, uint256 tokenId) public view virtual returns (MarketItem memory) {
+        uint itemCount = _itemIds.current();
+
+        MarketItem memory item;
+
+        for (uint i = 0; i < itemCount; i++) {
+            if (idToMarketItem[i + 1].nftContract == nftContractAddress && idToMarketItem[i + 1].tokenId == tokenId && idToMarketItem[i + 1].owner == address(0)) {
+                NftInterface nftContract = NftInterface(idToMarketItem[i + 1].nftContract);
+                address nftOwner = nftContract.ownerOf(idToMarketItem[i + 1].tokenId);
+
+                if (idToMarketItem[i + 1].owner == address(0) && nftOwner == idToMarketItem[i + 1].seller) {
+                    item = idToMarketItem[i + 1];
+                    break;
+                }
+            }
+        }
+
+        return item;
+    }
+
+    function fetchMarketItems() public view virtual returns (MarketItem[] memory) {
+        uint itemCount = _itemIds.current();
+        uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+        for (uint i = 0; i < itemCount; i++) {
+            NftInterface nftContract = NftInterface(idToMarketItem[i + 1].nftContract);
+            address nftOwner = nftContract.ownerOf(idToMarketItem[i + 1].tokenId);
+
+            if (idToMarketItem[i + 1].owner == address(0) && nftOwner == idToMarketItem[i + 1].seller) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    function fetchMyNFTs() public view virtual returns (MarketItem[] memory) {
+        uint totalItemCount = _itemIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                uint currentId = idToMarketItem[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    function version() pure public virtual returns (string memory) {
+        return "v1!";
+    }
+}
+
+contract MarketplaceV2 is Marketplace {
+    function version() pure public override returns (string memory) {
+        return "v2!";
+    }
+}
