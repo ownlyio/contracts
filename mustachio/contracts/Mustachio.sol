e2324a14a5f686bed9a03ca77190ef53f1da7b3b
Bernard Historillo <bernardhistorillo1@gmail.com>
2021-08-21 18:31:51 +0800

    Add totalSupply() and getLastMintedTokenId() to Mustachio contract

--- mustachio/contracts/Mustachio.sol
+++ mustachio/contracts/Mustachio.sol
@@ -2,25 +2,22 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Mustachio is ERC721, ERC721URIStorage, ReentrancyGuard, Ownable {
+contract Mustachio is ERC721Enumerable, ReentrancyGuard, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private tokenIds;
     
-    uint mintPrice = 0.3 ether;
-    string baseUri = "ownly.io/nft/mustachio/api/";
+//    uint mintPrice = 0.3 ether;
+    uint mintPrice = 0.01 ether;
+    string baseUri = "https://ownly.tk/api/mustachio/";
     // address payable admin = payable(0x672b733C5350034Ccbd265AA7636C3eBDDA2223B);
-    address payable admin = payable(0x88A14AF453b14070B9B943eea32bf3F534dFa01a);
+    address payable admin = payable(0x768532c218f4f4e6E4960ceeA7F5a7A947a1dd61);
     
     constructor() ERC721("Mustachio", "MUSTACHIO") {}
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
 
     function _baseURI() internal view override returns (string memory) {
         return baseUri;
@@ -30,10 +27,6 @@ contract Mustachio is ERC721, ERC721URIStorage, ReentrancyGuard, Ownable {
         baseUri = _baseUri;
     }
     
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-    
     function getMintPrice() public view returns (uint) {
         return mintPrice;
     }
@@ -41,9 +34,13 @@ contract Mustachio is ERC721, ERC721URIStorage, ReentrancyGuard, Ownable {
     function setMintPrice(uint _mintPrice) public onlyOwner {
         mintPrice = _mintPrice;
     }
-    
-    function createToken() public virtual payable nonReentrant {
-        require(msg.value == mintPrice, "Please submit the asking price in order to complete the purchase");
+
+    function getLastMintedTokenId() public view returns (uint) {
+        return tokenIds.current();
+    }
+
+    function mintMustachio() public virtual payable nonReentrant {
+        require(msg.value == mintPrice, "Please submit the asking price in order to complete the purchase.");
         tokenIds.increment();
         uint tokenId = tokenIds.current();
 
--- mustachio/scripts/mustachio-script.js
+++ mustachio/scripts/mustachio-script.js
@@ -1,17 +1,28 @@
 const hre = require("hardhat");
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
   const MustachioContract = await hre.ethers.getContractFactory("Mustachio");
   const mustachio = await MustachioContract.deploy();
   console.log("Mustachio Contract deployed to:", mustachio.address);
 
-  // await mustachio.createToken({ value: "10000000000000000" });
+  // console.log(await mustachio.functions);
+  //
+  // await mustachio.mintMustachio({value: "10000000000000000"});
   // console.log(await mustachio.tokenURI(1));
+  //
+  // await mustachio.setBaseUri("Hello World/");
+  // console.log(await mustachio.tokenURI(1));
+  //
+  // await mustachio.transferFrom(deployer.address, "0x000000000000000000000000000000000000dead", 1);
+  //
+  // console.log(parseInt(await mustachio.totalSupply()));
 
-  await hre.run("verify:verify", {
-      address: mustachio.address,
-      contract: "contracts/Mustachio.sol:Mustachio",
-  });
+  // await hre.run("verify:verify", {
+  //     address: mustachio.address,
+  //     contract: "contracts/Mustachio.sol:Mustachio",
+  // });
 }
 
 main()
