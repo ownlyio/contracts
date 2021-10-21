fae210316207621258db183f0667775254fbbc2f
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2021-10-21 21:15:02 +0800

    Add contracts for bridging

---
+++ bridge/scripts/arguments/TokenProxy.js
@@ -0,0 +1,7 @@
+module.exports = [
+    "0x37F1D763FaEE9D6ee1E3E97F180A9108A62EB0bC",
+    "Wrapped Ownly",
+    "wOWN",
+    18,
+    4
+];
---
+++ bridge/scripts/ethToken.js
@@ -0,0 +1,28 @@
+const hre = require("hardhat");
+let argumentPermittableToken = require("bridge/scripts/arguments/PermittableToken");
+let argumentTokenProxy = require("bridge/scripts/arguments/TokenProxy");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deployer:", deployer.address);
+
+    const PermittableToken = await hre.ethers.getContractFactory("PermittableToken");
+    const permittableToken = await PermittableToken.deploy(argumentPermittableToken);
+    console.log("Permittable Token Contract deployed to:", permittableToken.address);
+
+    argumentTokenProxy[0] = permittableToken.address;
+
+    const TokenProxy = await hre.ethers.getContractFactory("TokenProxy");
+    const tokenProxy = await TokenProxy.deploy(permittableToken.address, argumentTokenProxy);
+    console.log("Token Proxy Contract deployed to:", tokenProxy.address);
+
+    // await genesisBlock.mintMultiple("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", 100);
+    // console.log(await genesisBlock.tokenURI(4));
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
