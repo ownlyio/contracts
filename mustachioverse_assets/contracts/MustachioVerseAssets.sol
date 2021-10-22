e5df449864d751b749aa0c9e75c82d7de8e5923f
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-10-22 22:38:00 +0800

    Remove uinttostr util function and use Strings

--- mustachioverse_assets/contracts/MustachioVerseAssets.sol
+++ mustachioverse_assets/contracts/MustachioVerseAssets.sol
@@ -169,41 +169,16 @@ contract MustachioVerseAssets is ERC1155, ReentrancyGuard, Ownable {
         mintedPerToken[_groupId][_tokenId]++;
     }
 
-    // utils
-    function uint2str(uint256 _i)
-        internal
-        pure
-        returns (string memory _uintAsString)
-    {
-        if (_i == 0) {
-            return "0";
-        }
-        uint256 j = _i;
-        uint256 len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint256 k = len;
-        while (_i != 0) {
-            k = k - 1;
-            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
-            bytes1 b1 = bytes1(temp);
-            bstr[k] = b1;
-            _i /= 10;
-        }
-
-        return string(bstr);
-    }
-
+    // util
     function getGroupRange(uint256 _groupId)
         public
         view
         returns (string memory)
     {
-        string memory fromStrId = uint2str(mintTokenDetails[_groupId].fromId);
-        string memory toStrId = uint2str(mintTokenDetails[_groupId].to);
+        string memory fromStrId = Strings.toString(
+            mintTokenDetails[_groupId].fromId
+        );
+        string memory toStrId = Strings.toString(mintTokenDetails[_groupId].to);
 
         return string(abi.encodePacked(fromStrId, " ", toStrId));
     }
