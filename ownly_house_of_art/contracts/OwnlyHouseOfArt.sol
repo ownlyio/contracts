bd06a12489ce45cfebf03293a630c666127298ba
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2021-11-22 13:39:13 +0800

    Update OwnlyHouseOfArt contract

--- ownly_house_of_art/contracts/OwnlyHouseOfArt.sol
+++ ownly_house_of_art/contracts/OwnlyHouseOfArt.sol
@@ -3,11 +3,10 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OwnlyHouseOfArt is ERC721Enumerable, ReentrancyGuard, Ownable {
+contract OwnlyHouseOfArt is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private tokenIds;
 
