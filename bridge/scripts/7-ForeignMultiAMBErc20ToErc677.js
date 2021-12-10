ffb16c1d8a30d0f40eb1fbc76f95814755fa9c1b
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2021-12-10 16:22:43 +0800

    Update ForeignMultiAMBerc20ToErc677 contract deployment scripts

--- bridge/scripts/7-ForeignMultiAMBErc20ToErc677.js
+++ bridge/scripts/7-ForeignMultiAMBErc20ToErc677.js
@@ -15,39 +15,40 @@ async function main() {
     await eternalStorageProxy.upgradeTo(1, foreignMultiAMBErc20ToErc677.address);
 
     // Initialize
-    // let _bridgeContract = ""; // (4. Foreign AMB))
-    // let _mediatorContract = ""; // (6. HOMEMULTIAMBERC20TOERC677)
-    // let _token = ""; // (0. Main Token Contract)
-    //
-    // let ForeignMultiAMBErc20ToErc677Proxy = await ForeignMultiAMBErc20ToErc677.attach(eternalStorageProxy.address);
-    // await ForeignMultiAMBErc20ToErc677Proxy.initialize(
-    //     _bridgeContract,
-    //     _mediatorContract,
-    //     [
-    //         "15000000000000000000000000",
-    //         "750000000000000000000000",
-    //         "500000000000000000"
-    //     ],
-    //     [
-    //         "30000000000000000000000000",
-    //         "1500000000000000000000000",
-    //         "2000000"
-    //     ],
-    //     deployer.address
-    // );
-    // console.log("ForeignMultiAMBErc20ToErc677 Contract Initialized\n");
-    //
-    // await ForeignMultiAMBErc20ToErc677Proxy.setDailyLimit(_token, 75000000000000000000000000);
-    // console.log("ForeignMultiAMBErc20ToErc677 Contract setDailyLimit\n");
-    //
-    // await ForeignMultiAMBErc20ToErc677Proxy.setExecutionDailyLimit(_token, 75000000000000000000000000);
-    // console.log("ForeignMultiAMBErc20ToErc677 Contract setExecutionDailyLimit\n");
-    //
-    // await ForeignMultiAMBErc20ToErc677Proxy.setMaxPerTx(_token, 7500000000000000000000000);
-    // console.log("ForeignMultiAMBErc20ToErc677 Contract setMaxPerTx\n");
-    //
-    // await ForeignMultiAMBErc20ToErc677Proxy.setExecutionMaxPerTx(_token, 7500000000000000000000000);
-    // console.log("ForeignMultiAMBErc20ToErc677 Contract setExecutionMaxPerTx\n");
+    let this_address = "0x06F624C9d9448cECf80D0463B75678A49ff88a9a"; // (7. FOREIGNMULTIAMBERC20TOERC677)
+    let _bridgeContract = "0x8aA2014CcB4402Fbeb980cB27170C32c65AF8543"; // (4. Foreign AMB))
+    let _mediatorContract = "0x20066214fAf84E99C9586602ae35FD43D2254183"; // (6. HOMEMULTIAMBERC20TOERC677)
+    let _token = "0xC3Df366fAf79c6Caff3C70948363f00b9Ac55FEE"; // (0. Main Token Contract)
+
+    let ForeignMultiAMBErc20ToErc677Proxy = await ForeignMultiAMBErc20ToErc677.attach(this_address);
+    await ForeignMultiAMBErc20ToErc677Proxy.initialize(
+        _bridgeContract,
+        _mediatorContract,
+        [
+            "15000000000000000000000000",
+            "750000000000000000000000",
+            "500000000000000000"
+        ],
+        [
+            "30000000000000000000000000",
+            "1500000000000000000000000",
+        ],
+        2000000,
+        deployer.address
+    );
+    console.log("ForeignMultiAMBErc20ToErc677 Contract Initialized");
+
+    await ForeignMultiAMBErc20ToErc677Proxy.setDailyLimit(_token, "75000000000000000000000000");
+    console.log("ForeignMultiAMBErc20ToErc677 Contract setDailyLimit");
+
+    await ForeignMultiAMBErc20ToErc677Proxy.setExecutionDailyLimit(_token, "75000000000000000000000000");
+    console.log("ForeignMultiAMBErc20ToErc677 Contract setExecutionDailyLimit");
+
+    await ForeignMultiAMBErc20ToErc677Proxy.setMaxPerTx(_token, "7500000000000000000000000");
+    console.log("ForeignMultiAMBErc20ToErc677 Contract setMaxPerTx");
+
+    await ForeignMultiAMBErc20ToErc677Proxy.setExecutionMaxPerTx(_token, "7500000000000000000000000");
+    console.log("ForeignMultiAMBErc20ToErc677 Contract setExecutionMaxPerTx");
 }
 
 main()
