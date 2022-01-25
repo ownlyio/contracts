d1b481219b1fc0fddb67d83272b87837445f0f22
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-01-26 00:21:41 +0800

    Add ethToOWNConversion() method

--- marketplace/contracts/MarketplaceV2.sol
+++ marketplace/contracts/MarketplaceV2.sol
@@ -3,7 +3,101 @@ pragma solidity 0.8.2;
 
 import "./Marketplace.sol";
 
+abstract contract PancakeSwapRouterInterface {
+    function getAmountsIn(uint amountOut, address[] memory path) public view virtual returns (uint[] memory);
+}
+
 contract MarketplaceV2 is Marketplace {
+    event MarketItemPaid (
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        uint256 price,
+        string currency,
+        uint256 listingPrice
+    );
+
+    function createMarketSale(uint256 nftChainID, uint256 itemId, string memory currency) public virtual payable nonReentrant returns (uint) {
+        if(nftChainID == 56) {
+            address payable seller = idToMarketItem[itemId].seller;
+            uint price = idToMarketItem[itemId].price;
+            uint tokenId = idToMarketItem[itemId].tokenId;
+            address ownly_address = 0xC3Df366fAf79c6Caff3C70948363f00b9Ac55FEE;
+            //        address ownly_address = 0x7665CB7b0d01Df1c9f9B9cC66019F00aBD6959bA;
+
+            if(compareStrings(currency, "BNB") && compareStrings(idToMarketItem[itemId].currency, "BNB")) {
+                require(msg.value == price, "Please submit the asking price in order to complete the purchase");
+                seller.transfer(msg.value);
+            }
+
+            if(compareStrings(currency, "OWN") && compareStrings(idToMarketItem[itemId].currency, "OWN")) {
+                OwnlyInterface ownlyContract = OwnlyInterface(ownly_address);
+                uint ownlyAllowance = ownlyContract.allowance(msg.sender, address(this));
+
+                require(idToMarketItem[itemId].price == ownlyAllowance, "Please submit the asking price in order to complete the purchase");
+
+                IERC20Upgradeable(ownly_address).transferFrom(msg.sender, seller, idToMarketItem[itemId].price);
+            }
+
+            if(compareStrings(currency, "OWN") && compareStrings(idToMarketItem[itemId].currency, "BNB")) {
+                SparkSwapRouterInterface sparkSwapRouterContract = SparkSwapRouterInterface(0xeB98E6e5D34c94F56708133579abB8a6A2aC2F26);
+
+                address[] memory path = new address[](2);
+                path[0] = ownly_address;
+                path[1] = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+
+                uint[] memory ownPrice = sparkSwapRouterContract.getAmountsIn(price, path);
+
+                OwnlyInterface ownlyContract = OwnlyInterface(ownly_address);
+                uint ownlyAllowance = ownlyContract.allowance(msg.sender, address(this));
+
+                uint finalPrice = ownPrice[0];
+                finalPrice = (finalPrice * 8) / 10;
+
+                require(ownlyAllowance >= finalPrice, "Please submit the asking price in order to complete the purchase");
+
+                IERC20Upgradeable(ownly_address).transferFrom(msg.sender, seller, finalPrice);
+            }
+
+            IERC721Upgradeable(idToMarketItem[itemId].nftContract).transferFrom(seller, msg.sender, tokenId);
+            idToMarketItem[itemId].owner = payable(msg.sender);
+            _itemsSold.increment();
+
+            if(idToMarketItem[itemId].listingPrice > 0) {
+                payable(marketplaceOwner).transfer(idToMarketItem[itemId].listingPrice);
+            }
+
+            emit MarketItemSold(
+                itemId
+            );
+        } else if(nftChainID == 1) {
+            IERC20Upgradeable(ownly_address).transferFrom(msg.sender, seller, finalPrice);
+            payable(marketplaceOwner).transfer(idToMarketItem[itemId].listingPrice);
+
+
+        }
+
+        return 0;
+    }
+
+    function ethToOWNConversion(uint256 amount) public view virtual returns (uint256) {
+        PancakeSwapRouterInterface pancakeSwapRouterContract = PancakeSwapRouterInterface(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+
+        address[] memory path = new address[](2);
+        path[0] = 0x2170ed0880ac9a755fd29b2688956bd959f933f8; // BUSD
+        path[1] = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c; // ETH
+
+        uint[] memory busdPrice = pancakeSwapRouterContract.getAmountsIn(price, path);
+
+        path[0] = 0x7665CB7b0d01Df1c9f9B9cC66019F00aBD6959bA; // OWN
+        path[1] = 0xe9e7cea3dedca5984780bafc599bd69add087d56; // BUSD
+
+        uint[] memory ownPrice = pancakeSwapRouterContract.getAmountsIn(busdPrice[0], path);
+
+        return ownPrice[0];
+    }
+
     function version() pure public virtual override returns (string memory) {
         return "v2";
     }
--- marketplace/scripts/deploy_marketplacev2.js
+++ marketplace/scripts/deploy_marketplacev2.js
@@ -3,26 +3,30 @@ const { ethers, upgrades } = require('hardhat');
 async function main () {
     const [deployer] = await ethers.getSigners();
 
+    const Marketplace = await ethers.getContractFactory('Marketplace');
+
+    console.log('Deploying Marketplace...');
+
+    const marketplace = await upgrades.deployProxy(Marketplace, { kind: 'uups' });
+    await marketplace.deployed();
+
+    // MarketplaceV2 Deployment
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
     console.log('Upgrading to MarketplaceV2...');
     const MarketplaceV2 = await ethers.getContractFactory('MarketplaceV2');
-    const marketplace = await upgrades.upgradeProxy('0x3DA2Af334F3BC6f80Fcad3268441455171A0f53B', MarketplaceV2);
+    const marketplacev2 = await upgrades.upgradeProxy('0x3DA2Af334F3BC6f80Fcad3268441455171A0f53B', MarketplaceV2);
 
     const implHex = await ethers.provider.getStorageAt(
-        marketplace.address,
+        marketplacev2.address,
         "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
     );
     const implAddress = ethers.utils.hexStripZeros(implHex);
     console.log('Implementation Address: ', implAddress);
 
-    // await hre.run("verify:verify", {
-    //     address: implAddress
-    // });
-
-    console.log('Marketplace deployed to: ', marketplace.address);
-    console.log('Version: ', await marketplace.version());
+    console.log('Marketplace deployed to: ', marketplacev2.address);
+    console.log('Version: ', await marketplacev2.version());
 }
 
 main()
